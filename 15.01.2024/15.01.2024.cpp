8f32397d0a7080cd4ac7c1a9ed7a620160a22b61
DeanSergeyevich <109577115+DeanSergeyevich@users.noreply.github.com>
2024-01-15 19:47:30 +0300

    Add files via upload

---
+++ 15.01.2024/15.01.2024.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+class Student 
+{
+private:
+	string name;
+	int age;
+
+public:
+	Student(string name, int age)
+	{
+		this->age = age;
+		this->name = name;
+	}
+	void show()
+	{
+		cout << "name: " << name << " age: " << age << endl;
+	}
+
+};
+
+
+int main()
+{
+	Student st1("Pol", 23);
+	st1.show();
+}
