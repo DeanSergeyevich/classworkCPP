dc67a91f8fd17d5619eabd3d2a870a123bd0dba3
DeanSergeyevich <109577115+DeanSergeyevich@users.noreply.github.com>
2024-01-22 21:11:40 +0300

    Add files via upload

---
+++ 22.01.2024/Lesson5.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <string>
+//#include <random>
+
+using namespace std;
+
+class Drob
+{
+    int num;
+    int denum;
+public:
+    Drob(int num, int denum)
+    {
+        this->num = num;
+        this->denum = denum;
+
+    }
+
+    Drob mult(Drob& drob)
+    {
+        int newNum = this->num * drob.num;
+        int newDenum = this->denum * drob.denum;
+        Drob newDrob(newNum, newDenum);
+        return newDrob;
+        
+    }
+    Drob operator *(Drob& drob)
+    {
+        int newNum = this->num * drob.num;
+        int newDenum = this->denum * drob.denum;
+        Drob newDrob(newNum, newDenum);
+        return newDrob;
+
+    }
+
+    void show()
+    {
+        cout << num << " / "
+    }
+
+
+};
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    Drob d1(2, 3);
+    Drob d2(4, 2);
+    Drob d3 = d1.mult(d2);
+    Drob d4 = d1 * d2;
+
+    d3.show();
+    d4.show();
+    d1.show();
+    d2.show();
+
+}
